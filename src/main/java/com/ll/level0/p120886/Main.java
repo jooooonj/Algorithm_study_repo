f152ab21de2b4ed7a9a767d566883b4bb7931663
jooooonj <lee22424205@naver.com>
2023-03-03 10:15:02 +0900

    20230303

---
+++ src/main/java/com/ll/level0/p120886/Main.java
@@ -0,0 +1,27 @@
+package com.ll.level0.p120886;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int solution(String before, String after) {
+        int[] alpha = new int[26];
+        int result = 1;
+        for(int i=0; i<before.length(); i++){
+            alpha[before.charAt(i) - 'a']++;
+            alpha[after.charAt(i)-'a']--;
+        }
+
+        for(int i=0; i<alpha.length; i++){
+            if (alpha[i] > 0) {
+                result = 0;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
