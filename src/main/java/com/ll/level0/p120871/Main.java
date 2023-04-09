24ce22061dbb734992dad839ffef1d917e28e165
jooooonj <lee22424205@naver.com>
2023-04-09 18:21:11 +0900

    저주의 숫자 3 수정

--- src/main/java/com/ll/level0/p120871/Main.java
+++ src/main/java/com/ll/level0/p120871/Main.java
@@ -11,24 +11,14 @@ public class Main {
 class Solution {
     public int solution(int n) {
         int num = 1;
-        int[] arr = new int[n];
         for(int i=0; i<n; i++){
-//            System.out.println("i = " + i);
-            String str = Integer.toString(num);
-            char ch = str.charAt(str.length() - 1);
 
-            while(num%3==0 || ch-'0' == 3 || (num-30 >=0 && num-30 <= 9)){
+            while(num%3==0 || Integer.toString(num).contains("3")){
                 num ++;
-                str = Integer.toString(num);
-                ch = str.charAt(str.length() - 1);
             }
-
-            arr[i] = num;
-//            System.out.println(num);
             num ++;
-//            System.out.println(Arrays.toString(arr));
         }
 
-        return arr[arr.length - 1];
+        return num -1 ;
     }
 }
