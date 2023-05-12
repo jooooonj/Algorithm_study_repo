c8ad33857bb5faaf2faf1abfc42e811b0e8331ba
jooooonj <lee22424205@naver.com>
2023-05-12 18:07:22 +0900

    [프로그래머스]/72412/순위검색/lev2/구현/카카오/효율성실패

---
+++ src/main/java/com/ll/programers/level2/p72412/Main.java
@@ -0,0 +1,84 @@
+package com.ll.programers.level2.p72412;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] info = {"java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"};
+        String[] query = {"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"};
+        s.solution(info, query);
+    }
+}
+
+class Solution {
+    class Person{
+        String lan;
+        String skill;
+        String level;
+        String cook;
+        String score;
+
+        Person(String lan, String skill, String level, String cook, String score){
+            this.lan = lan;
+            this.skill = skill;
+            this.level = level;
+            this.cook = cook;
+            this.score = score;
+        }
+
+        //주어진 조건에 일치하는지 검사하는 함수
+        boolean isCorrect(String lan, String skill, String level, String cook, String score){
+            if(lan.equals("-")) lan = this.lan;
+            if(skill.equals("-")) skill = this.skill;
+            if(level.equals("-")) level = this.level;
+            if(cook.equals("-")) cook = this.cook;
+            if(score.equals("-")) score = this.score;
+
+            if (this.lan.equals(lan) &&
+                    this.skill.equals(skill) &&
+                    this.level.equals(level) &&
+                    this.cook.equals(cook) &&
+                    Integer.parseInt(this.score) >= Integer.parseInt(score)) {
+                return true;
+            }
+            return false;
+        }
+    }
+    public int[] solution(String[] info, String[] query) {
+        List<Person> studentList = new ArrayList<>();
+        for(String str : info){
+            String[] split = str.split(" ");
+            String lan = split[0];
+            String skill = split[1];
+            String level = split[2];
+            String cook = split[3];
+            String score = split[4];
+            studentList.add(new Person(lan,skill,level,cook,score));
+        }
+
+        int[] result = new int[query.length];
+
+        for(int i=0; i<query.length; i++){
+            String q = query[i];
+            int count = 0; //몇명인지 셀 함수
+
+            String[] split = q.split(" ");
+            String lan = split[0];
+            String skill = split[2];
+            String level = split[4];
+            String cook = split[6];
+            String score = split[7];
+
+            for(Person person : studentList){
+                if(person.isCorrect(lan,skill,level,cook,score))
+                    count++;
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
