3bcad7e5717af6786dedc04a7f6b79f3403996ce
jooooonj <lee22424205@naver.com>
2023-08-07 12:15:20 +0900

    [BOJ]/2304/창고다각형/Silver2/R

--- src/main/java/com/ll/baekjoon/R_Baekjoon2304/Main.java
+++ src/main/java/com/ll/baekjoon/R_Baekjoon2304/Main.java
@@ -8,60 +8,70 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int L;
+    static int H;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        //제일 높은거 찾기
+        //좌,우에서 제일높은거까지 자기보다 높은거 찾기
+
+        N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][2];
-        for (int i = 0; i < N; i++) {
+        int max = 0;
+        int maxL = -1;
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
+
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
+            if(max < arr[i][1]){
+                max = arr[i][1];
+                maxL = arr[i][0];
+            }
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
+        Arrays.sort(arr, new Comparator<int[]>(){
+
+            public int compare(int[] a1, int[] a2){
+                return a1[0] - a2[0];
             }
         });
 
-        int total = 0;
-        int max = Integer.MIN_VALUE;
-        int maxIndex = -1;
+        int sum = max;
+        int m = arr[0][1];
+        int n = arr[0][0];
+        //왼쪽
         for(int i=0; i<N; i++){
-            if(max < arr[i][1]){
-                max = arr[i][1];
-                maxIndex = i;
+            if(m <= arr[i][1]){
+                sum += m * (arr[i][0] - n);
+                m = arr[i][1];
+                n = arr[i][0];
             }
-        }
-        total += max;
-
-        int standardX = arr[0][0];
-        int standardH = arr[0][1];
-
-        for(int i=0; i<=maxIndex; i++){
-            if(arr[i][1] >= standardH){
-                total += (arr[i][0] - standardX) * standardH;
-                standardX = arr[i][0];
-                standardH = arr[i][1];
+            if(arr[i][0] == maxL) {
+                break;
             }
+
         }
 
-        standardX = arr[N-1][0];
-        standardH = arr[N-1][1];
-        for(int i=N-1; i>=maxIndex; i--){
-            if(arr[i][1] >= standardH){
-                total += (standardX - arr[i][0]) * standardH;
-                standardX = arr[i][0];
-                standardH = arr[i][1];
+        //오른족
+        m = arr[N-1][1];
+        n = arr[N-1][0];
+        for(int i=N-1; i>=0; i--){
+            if(m <= arr[i][1]){
+                sum += m * (n - arr[i][0]);
+                m = arr[i][1];
+                n = arr[i][0];
             }
+
+            if(arr[i][0] == maxL) {
+                break;
+            }
+
         }
-        System.out.println(total);
 
-
-
-
+        System.out.println(sum);
     }
 }
