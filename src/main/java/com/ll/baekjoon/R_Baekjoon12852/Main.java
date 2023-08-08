8a4342d43c31833f633f01544f67ae8fc762f5fe
jooooonj <lee22424205@naver.com>
2023-08-08 12:01:49 +0900

    [BOJ]/12852/1로만들기2/Silver1/R

--- src/main/java/com/ll/baekjoon/R_Baekjoon12852/Main.java
+++ src/main/java/com/ll/baekjoon/R_Baekjoon12852/Main.java
@@ -6,41 +6,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
     static int[] dp;
-    static int[] node;
-    static String result = "";
-    static int n;
+    static int[] history;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n+1];
-        node = new int[n+1];
-        dp[1] = 0;
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + 1;
-            node[i] = i-1;
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N+1]; //count 세기
+        history = new int[N+1];
+        history[0] = 1;
+        for(int i=2; i<=N; i++){
+            dp[i] =  dp[i-1] + 1;
+            history[i] = i -1;
+
             if(i%2==0 && dp[i] > dp[i/2] + 1){
                 dp[i] = dp[i/2] + 1;
-                node[i] = i/2;
+                history[i] = i/2;
             }
 
             if(i%3==0 && dp[i] > dp[i/3] + 1){
                 dp[i] = dp[i/3] + 1;
-                node[i] = i/3;
+                history[i] = i/3;
             }
         }
 
+        sb.append(dp[N]+"\n"); //최소 count
 
-        int index = n;
-        sb.append(dp[n]+"\n");
-        sb.append(n+" ");
-        for(int i=0; i<dp[n]; i++){
-            sb.append(node[index]+" ");
-            index = node[index];
+        int num = N;
+        while(num >= 1){
+            sb.append(num+" ");
+            num = history[num];
         }
         System.out.println(sb);
+
+
+
+
+
     }
 }
