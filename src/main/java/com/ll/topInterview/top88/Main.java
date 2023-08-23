75f0fcdf76357cf0da178ca1fbfd3107ceaaf3ee
jooooonj <lee22424205@naver.com>
2023-08-23 11:41:27 +0900

    [Leetcode]/88/Merge_Sorted_Array

---
+++ src/main/java/com/ll/topInterview/top88/Main.java
@@ -0,0 +1,20 @@
+package com.ll.topInterview.top88;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=m; i< nums1.length; i++){
+
+            nums1[i] = nums2[i-m];
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
