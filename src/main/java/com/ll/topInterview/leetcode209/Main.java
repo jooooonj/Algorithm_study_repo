125b8f3dee4bb49185e79b684b7d5815f18b5815
jooooonj <lee22424205@naver.com>
2023-08-28 17:19:46 +0900

    [Leetcode]/209/Minimum Size Subarray Sum

---
+++ src/main/java/com/ll/topInterview/leetcode209/Main.java
@@ -0,0 +1,29 @@
+package com.ll.topInterview.leetcode209;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+
+        while(right < nums.length){
+            sum += nums[right++];
+
+            while(sum >= target){
+                min = Math.min(min, right - left);
+                sum -= nums[left++];
+            }
+        }
+
+        if(min==Integer.MAX_VALUE) return 0;
+        return min;
+    }
+}
