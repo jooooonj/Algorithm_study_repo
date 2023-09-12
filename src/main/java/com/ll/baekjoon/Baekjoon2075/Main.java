ed4dc07cc410a61e04d743eaa69dc8bb6252c415
jooooonj <lee22424205@naver.com>
2023-09-12 18:29:01 +0900

    [BOJ]/2075/N번째큰수/Silver2/우선순위큐/리팩토링

--- src/main/java/com/ll/baekjoon/Baekjoon2075/Main.java
+++ src/main/java/com/ll/baekjoon/Baekjoon2075/Main.java
@@ -3,8 +3,7 @@ package com.ll.baekjoon.Baekjoon2075;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,22 +12,24 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
-        int[] seq = new int[N*N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 낮은 수가 우선순위
+
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int index = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                seq[index++] = map[i][j];
-            }
-        }
-        Arrays.sort(seq);
 
-        System.out.println(seq[N*N - N ]);
+            for(int j=0; j<N; j++){
+                int num = Integer.parseInt(st.nextToken());
+
+                if(pq.size() == N){
+                    if(pq.peek() < num){
+                        pq.poll();
+                        pq.add(num);
+                    }
+                } else
+                    pq.add(num);
+            }
+        }
+
+        System.out.println(pq.peek());
     }
 }
