79c63b953087bddda4b192a00c5f2d0992c72a79
jooooonj <lee22424205@naver.com>
2023-09-12 19:19:44 +0900

    [BOJ]/15989/1,2,3 더하기4/Silver1/dp

---
+++ src/main/java/com/ll/baekjoon/Baekjoon15989/Main.java
@@ -0,0 +1,38 @@
+package com.ll.baekjoon.Baekjoon15989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[10001][4]; //1~3
+        dp[1][1] = 1; //1
+        dp[2][1] = 1; //1 1
+        dp[2][2] = 1; //2
+        dp[3][1] = 1; //1 1 1
+        dp[3][2] = 1; //1 2
+        dp[3][3] = 1; //3
+
+//        dp[4][1] = dp[3][1];
+//        dp[4][2] = dp[2][1] + dp[2][2];
+//        dp[4][3] = dp[1][1] + dp[1][2] + dp[1][3];
+
+        for(int i=4; i<dp.length; i++){
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+        }
+        for(int t=1; t<=T; t++){
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
