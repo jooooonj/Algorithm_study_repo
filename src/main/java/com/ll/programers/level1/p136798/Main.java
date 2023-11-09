3a26feea3613fac2307859c4c5204de58214a8c2
jooooonj <lee22424205@naver.com>
2023-11-09 14:09:02 +0900

    [PRO]/136798/lev1/기사단원의무기/약수구하기

---
+++ src/main/java/com/ll/programers/level1/p136798/Main.java
@@ -0,0 +1,30 @@
+package com.ll.programers.level1.p136798;
+
+public class Main {
+}
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int[] numArr = new int[number+1];
+
+        for(int i=1; i<=number; i++){
+            int num = i;
+            int count = 0;
+            for(int j=1; j*j<=num; j++){
+                if(j*j == num) count++;
+                else if(num % j == 0) count+=2;
+            }
+
+            numArr[i] = count;
+        }
+
+        int sum = 0;
+        for(int n : numArr){
+            if(n > limit) sum += power;
+            else
+                sum += n;
+        }
+
+        return sum;
+    }
+}
